9c20ab5e10defb082d9de4f1fafb8e3bce63efba
Valentina Myasnikova <troflia031288@gmail.com>
2025-12-05 14:06:55 +0300

    Получение суммы, разности, произведения и частного вводимых пользователем чисел

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in) .nextLine ();
-        System.out.println ("Длина текста: " + text.length());
+        System.out.println("Необходимо ввести два числа");
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in) .nextInt();
+
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in) .nextInt();
+
+        int sum = number1 + number2;
+        int raz = number1 - number2;
+        int umn = number1 * number2;
+        double del = (double) number1 / number2;
+
+        System.out.println("Сумма чисел: " + sum);
+        System.out.println("Разность чисел: " + raz);
+        System.out.println("Произведение чисел: " + umn);
+        System.out.println("Частное чисел: " + del);
     }
 }
