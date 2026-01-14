d90586e2ad19c5db0284868209f129e4d78a3ed4
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-14 14:37:15 +0300

    Практика ООП. Полиморфизм #1. Трехмерная точка

---
+++ src/Point.java
@@ -0,0 +1,17 @@
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Point3D extends Point {
+    int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y); // Вызываем конструктор класса Point
+        this.z = z;
+    }
+}
