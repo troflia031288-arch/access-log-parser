d26eb5142c144eaf8b7f3ed5aed9c7d9dab91f35
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-16 17:47:05 +0300

    Практика ООП. Полиморфизм #5. Дробь это число

---
+++ src/Fraction.java
@@ -0,0 +1,32 @@
+class Fraction extends Number {
+    int num, denum; //числитель и знаменатель
+
+    public Fraction(int num, int denum) {
+        if (denum == 0) {
+            throw new IllegalArgumentException("На 0 делить нельзя");
+        }
+        this.num = num;
+        this.denum = denum;
+    }
+
+    public String toString() {
+        return num + "/" + denum;
+    }
+
+    public double doubleValue() {
+        return (double) num / denum; // double
+    }
+
+    public float floatValue() {
+        return (float) num / denum; // float
+    }
+
+    public int intValue() {
+        return num / denum; // int
+    }
+
+    public long longValue() {
+        return num / denum; // long
+    }
+
+}
