5b86fbe6c8b371b02922f55b19c0dde01096ee8a
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-19 10:08:14 +0300

    Практика ООП. Полиморфизм #3. Измерение длины. Учли требования инкапсуляции

--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,18 @@
 class Point {
 
-    int x, y;
+    private int x;
+    private int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
--- src/PolyLine.java
+++ src/PolyLine.java
@@ -3,8 +3,8 @@ interface Measurable {
 }
 
 class Line implements Measurable {
-    Point start;
-    Point end;
+    private Point start;
+    private Point end;
 
     public Line(Point start, Point end) {
         this.start = start;
@@ -13,15 +13,15 @@ class Line implements Measurable {
 
     @Override
     public double getLength() {
-        double lenX = start.x - end.x;
-        double lenY = start.y - end.y;
+        double lenX = start.getX() - end.getX();
+        double lenY = start.getY() - end.getY();
         return Math.sqrt(lenX * lenX + lenY * lenY);
     }
 }
 
 class PolyLine implements Measurable {
 
-    Point[] points;
+    protected Point[] points;
 
     public PolyLine(Point... points) {
         this.points = points;
@@ -29,11 +29,11 @@ class PolyLine implements Measurable {
 
     @Override
     public double getLength() {
-        double sum = 0, len1, len2;
+        double sum = 0;
         for (int i = 0; i < points.length - 1; i++) {
-            len1 = points[i].x - points[i + 1].x;
-            len2 = points[i].y - points[i + 1].y;
-            sum += Math.sqrt(len1 * len1 + len2 * len2);
+            double lenX = points[i].getX() - points[i + 1].getX();
+            double lenY = points[i].getY() - points[i + 1].getY();
+            sum += Math.sqrt(lenX * lenX + lenY * lenY);
         }
         return sum;
     }
@@ -51,9 +51,9 @@ class ClosedPolyLine extends PolyLine {
             Point firstPoint = points[0];
             Point lastPoint = points[points.length - 1];
 
-            double len1 = firstPoint.x - lastPoint.x;
-            double len2 = firstPoint.y - lastPoint.y;
-            double Length2 = Math.sqrt(len1 * len1 + len2 * len2);
+            double lenX = firstPoint.getX() - lastPoint.getX();
+            double lenY = firstPoint.getY() - lastPoint.getY();
+            double Length2 = Math.sqrt(lenX * lenX + lenY * lenY);
 
             return Length1 + Length2;
         }
