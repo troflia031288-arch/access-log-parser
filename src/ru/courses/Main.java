3a375946f9e8631034ddb206f391ef5babcbc0d6
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-20 10:41:36 +0300

    Практика ООП. Пакеты #1. Навести порядок

---
+++ src/ru/courses/Main.java
@@ -0,0 +1,25 @@
+import ru.courses.geometry.ClosedPolyLine;
+import ru.courses.geometry.Line;
+import ru.courses.geometry.Measurable;
+import ru.courses.geometry.PolyLine;
+import ru.courses.geometry.Point;
+
+
+public class Main {
+    public static void printLengths(Measurable[] measurables) {
+        for (Measurable measurable : measurables) {
+            System.out.println("Длина =  " + measurable.getLength());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Создаем объекты
+        Line line = new Line(new Point(1, 2), new Point(5, 6));
+        PolyLine polyline = new PolyLine(new Point(1, 2), new Point(5, 6), new Point(10, 11));
+        ClosedPolyLine closedPolyline = new ClosedPolyLine(new Point(0, 0), new Point(3, 4), new Point(6, 0));
+
+        // Передаем их в метод printLengths
+        Measurable[] shapes = {line, polyline, closedPolyline};
+        printLengths(shapes);
+    }
+}
---
+++ src/ru/courses/Measurable.java
@@ -0,0 +1,6 @@
+package ru.courses.geometry;
+
+public interface Measurable {
+    double getLength();
+}
+
