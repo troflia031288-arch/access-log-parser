510384719da3ef95e7135c8d642e142189b73fb3
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-23 21:44:02 +0300

    Практика ООП. Класс Object #4. Сравнение ломаных линий

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -2,32 +2,25 @@ package ru.courses.main;
 
 import ru.courses.geometry.Point;
 import ru.courses.geometry.Line;
+import ru.courses.geometry.PolyLine;
 
 public class Main {
     public static void main(String[] args) {
 
         Point p1 = new Point(4, 6);
         Point p2 = new Point(7, 2);
+        Point p3 = new Point(1, 3);
 
-        Line line1 = new Line(p1, p2);
+        PolyLine polyline1 = new PolyLine(p1, p2, p3);
+        PolyLine polyline2 = new PolyLine(p1, p2, p3);
+        PolyLine polyline3 = new PolyLine(p1, p2);
 
-        Line line2 = null;
-        try {
-            line2 = (Line) line1.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Равна ли ломаная polyline1 ломаной polyline2? " + polyline1.equals(polyline2));
+        System.out.println("Равна ли ломаная polyline1 ломаной polyline3? " + polyline1.equals(polyline3));
 
-        System.out.println("Линия 1: " + line1);
-        System.out.println("Линия 2: " + line2);
-        System.out.println("Линия 1 равна линии 2? " + line1.equals(line2));
-
-        try {
-            Point newEnd = new Point(8, 2);
-            line2 = new Line(p1, newEnd);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Хэш-код ломаной polyline1: " + polyline1.hashCode());
+        System.out.println("Хэш-код ломаной polyline2: " + polyline2.hashCode());
+        System.out.println("Хэш-код ломаной polyline3: " + polyline3.hashCode());
 
     }
 }
