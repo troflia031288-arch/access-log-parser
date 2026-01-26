70919fefa88566fc9e65bde8a52d19445fd8da16
Valentina Myasnikova <troflia031288@gmail.com>
2026-01-26 14:07:32 +0300

    Практика ООП. Перечисления #1. Соус

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -1,26 +1,15 @@
 package ru.courses.main;
 
-import ru.courses.geometry.Point;
-import ru.courses.geometry.Line;
-import ru.courses.geometry.PolyLine;
+import ru.courses.product.Sauce;
 
 public class Main {
     public static void main(String[] args) {
+        Sauce sauce1 = new Sauce("Бастако", Sauce.Spiciness.VERY_HOT);
+        Sauce sauce2 = new Sauce("Юдзу", Sauce.Spiciness.HOT);
+        Sauce sauce3 = new Sauce("Сырный", Sauce.Spiciness.MILD);
 
-        Point p1 = new Point(4, 6);
-        Point p2 = new Point(7, 2);
-        Point p3 = new Point(1, 3);
-
-        PolyLine polyline1 = new PolyLine(p1, p2, p3);
-        PolyLine polyline2 = new PolyLine(p1, p2, p3);
-        PolyLine polyline3 = new PolyLine(p1, p2);
-
-        System.out.println("Равна ли ломаная polyline1 ломаной polyline2? " + polyline1.equals(polyline2));
-        System.out.println("Равна ли ломаная polyline1 ломаной polyline3? " + polyline1.equals(polyline3));
-
-        System.out.println("Хэш-код ломаной polyline1: " + polyline1.hashCode());
-        System.out.println("Хэш-код ломаной polyline2: " + polyline2.hashCode());
-        System.out.println("Хэш-код ломаной polyline3: " + polyline3.hashCode());
-
+        System.out.println(sauce1);
+        System.out.println(sauce2);
+        System.out.println(sauce3);
     }
 }
