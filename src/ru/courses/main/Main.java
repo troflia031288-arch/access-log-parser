78cf56debd99a4ed0fe588e7f5ff304e57e65610
Valentina Myasnikova <troflia031288@gmail.com>
2026-02-05 23:28:55 +0300

    Курсовой проект. Задание #2 по теме "Collections"

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -1,17 +1,21 @@
 package ru.courses.main;
 import ru.courses.product.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics();
         
-        stats.addEntry("https://testpage.ru/num1", 401, "MacOS");
-        stats.addEntry("https://testpage.ru/num2", 200, "MacOS");
-        stats.addEntry("https://testpage.ru/num3", 403, "Windows");
-        stats.addEntry("https://testpage.ru/num4", 500, "Linux");
+        stats.addEntry("https://testpage.ru/num1", 401, "Chrome");
+        stats.addEntry("https://testpage.ru/num2", 200, "Firefox");
+        stats.addEntry("https://testpage.ru/num3", 404, "Chrome");
+        stats.addEntry("https://testpage.ru/num4", 500, "Yandex");
+        stats.addEntry("https://testpage.ru/num5", 404, "Internet Explorer");
 
-        System.out.println("Список страниц с кодом 200: " + stats.getAllPages());
-        System.out.println("Статистика операционных систем: " + stats.getOSStatistics());
+        System.out.println("Список страниц с кодом 404: " + stats.getAbsentPages());
+        System.out.println("Статистика браузеров: " + stats.getBrowserStatistics());
+        
+
     }
 
 }
--- src/ru/courses/product/Statistics.java
+++ src/ru/courses/product/Statistics.java
@@ -3,49 +3,50 @@ package ru.courses.product;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Statistics {
-    private HashSet<String> pages;
-    private HashMap<String, Integer> numberOfOs;
+    private Set<String> absentPages;
+    private Map<String, Integer> browserCounts;
 
     public Statistics() {
-        pages = new HashSet<>();
-        numberOfOs = new HashMap<>();
+        absentPages = new HashSet<>();
+        browserCounts = new HashMap<>();
     }
 
-    public void addEntry(String url, int responseCode, String os) {
-        if (responseCode == 200) {
-            pages.add(url);
+    public void addEntry(String url, int responseCode, String browser) {
+        if (responseCode == 404) {
+            absentPages.add(url);
         }
 
-        if (os != null) {
-            numberOfOs.put(os, numberOfOs.getOrDefault(os, 0) + 1);
+        if (browser != null) {
+            browserCounts.put(browser, browserCounts.getOrDefault(browser, 0) + 1);
         }
     }
 
-    public HashSet<String> getAllPages() {
-        return pages;
+    public Set<String> getAbsentPages() {
+        return absentPages;
     }
 
-    public HashMap<String, Double> getOSStatistics() {
-        HashMap<String, Double> osEntry = new HashMap<>();
+    public Map<String, Double> getBrowserStatistics() {
+        Map<String, Double> browserStatistics = new HashMap<>();
         int totalNum = 0;
 
-        for (int count : numberOfOs.values()) {
+        for (int count : browserCounts.values()) {
             totalNum += count;
         }
 
         if (totalNum == 0) {
-            return osEntry;
+            return browserStatistics;
         }
 
-        for (Map.Entry<String, Integer> entry : numberOfOs.entrySet()) {
-            String osName = entry.getKey();
+        for (Map.Entry<String, Integer> entry : browserCounts.entrySet()) {
+            String browserName = entry.getKey();
             int count = entry.getValue();
             double proportion = (double) count / totalNum;
-            osEntry.put(osName, proportion);
+            browserStatistics.put(browserName, proportion);
         }
 
-        return osEntry;
+        return browserStatistics;
     }
 }
