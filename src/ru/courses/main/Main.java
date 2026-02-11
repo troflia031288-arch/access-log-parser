fdfe5c02e412f21aafb6e878a917d222cf953989
Valentina Myasnikova <troflia031288@gmail.com>
2026-02-11 23:33:51 +0300

    Курсовой проект. Задание #1 по теме "Stream API"

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -1,21 +1,24 @@
 package ru.courses.main;
-import ru.courses.product.*;
 
+import ru.courses.product.*;
 
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics();
-        
-        stats.addEntry("https://testpage.ru/num1", 401, "Chrome");
-        stats.addEntry("https://testpage.ru/num2", 200, "Firefox");
-        stats.addEntry("https://testpage.ru/num3", 404, "Chrome");
-        stats.addEntry("https://testpage.ru/num4", 500, "Yandex");
-        stats.addEntry("https://testpage.ru/num5", 404, "Internet Explorer");
+
+        stats.addEntry("https://testpage.ru/num1", 401, "Chrome", "192.168.1.1");
+        stats.addEntry("https://testpage.ru/num2", 200, "Firefox", "192.168.1.2");
+        stats.addEntry("https://testpage.ru/num3", 404, "Chrome", "192.168.1.1");
+        stats.addEntry("https://testpage.ru/num4", 500, "Yandex", "192.168.1.3");
+        stats.addEntry("https://testpage.ru/num5", 404, "Internet Explorer", "192.168.1.1");
 
         System.out.println("Список страниц с кодом 404: " + stats.getAbsentPages());
         System.out.println("Статистика браузеров: " + stats.getBrowserStatistics());
-        
 
+        stats.setTimePeriod(3.87); // Указываем время в часах, за которое есть записи в логах
+
+        System.out.println("Среднее количество посещений сайта за час: " + stats.averageVisitsPerHour());
+        System.out.println("Среднее количество ошибочных запросов за час: " + stats.averageErrorRequestsPerHour());
+        System.out.println("Средняя посещаемость одним пользователем: " + stats.averageVisitsPerUser());
     }
-
 }
--- src/ru/courses/product/Statistics.java
+++ src/ru/courses/product/Statistics.java
@@ -7,20 +7,36 @@ import java.util.Set;
 
 public class Statistics {
     private Set<String> absentPages;
-    private Map<String, Integer> browserCounts;
+    private Map<String, Integer> browserNum;
+    private Set<String> uniqueUserIPs; // уникальные IP не ботов
+    private int totalVisits; // кол-во посещений
+    private double hours;   // период времени в часах
+    private int errorRequestsNum; // кол-во ошибочных запросов
 
     public Statistics() {
         absentPages = new HashSet<>();
-        browserCounts = new HashMap<>();
+        browserNum = new HashMap<>();
+        uniqueUserIPs = new HashSet<>();
+        totalVisits = 0;
+        hours = 0;
+        errorRequestsNum = 0;
     }
 
-    public void addEntry(String url, int responseCode, String browser) {
+    public void addEntry(String url, int responseCode, String browser, String userIP) {
         if (responseCode == 404) {
             absentPages.add(url);
         }
 
+        if (responseCode >= 400 && responseCode < 600) {
+            errorRequestsNum++;
+        }
+
         if (browser != null) {
-            browserCounts.put(browser, browserCounts.getOrDefault(browser, 0) + 1);
+            browserNum.put(browser, browserNum.getOrDefault(browser, 0) + 1);
+            if (!browser.toLowerCase().contains("bot")) {
+                totalVisits++;
+                uniqueUserIPs.add(userIP);
+            }
         }
     }
 
@@ -32,7 +48,7 @@ public class Statistics {
         Map<String, Double> browserStatistics = new HashMap<>();
         int totalNum = 0;
 
-        for (int count : browserCounts.values()) {
+        for (int count : browserNum.values()) {
             totalNum += count;
         }
 
@@ -40,13 +56,36 @@ public class Statistics {
             return browserStatistics;
         }
 
-        for (Map.Entry<String, Integer> entry : browserCounts.entrySet()) {
+        for (Map.Entry<String, Integer> entry : browserNum.entrySet()) {
             String browserName = entry.getKey();
             int count = entry.getValue();
             double proportion = (double) count / totalNum;
             browserStatistics.put(browserName, proportion);
         }
-
         return browserStatistics;
+    }
+
+    public void setTimePeriod(double hours) { // Изменено на double
+        this.hours = hours;
+    }
+
+    public double averageVisitsPerHour() {
+        if (hours == 0) {
+            return 0;
+        }
+        return totalVisits / hours; // Используем double для деления
+    }
+
+    public double averageErrorRequestsPerHour() {
+        if (hours == 0) {
+            return 0;
+        }
+        return errorRequestsNum / hours; // Используем double для деления
+    }
+
+    public double averageVisitsPerUser() {        if (uniqueUserIPs.isEmpty()) {
+        return 0;
+    }
+        return (double) totalVisits / uniqueUserIPs.size();
     }
 }
