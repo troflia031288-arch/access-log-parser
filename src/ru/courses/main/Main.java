9e5170e80720490bab71765cb4f65897f18f961f
Valentina Myasnikova <troflia031288@gmail.com>
2026-02-12 23:46:12 +0300

    Курсовой проект. Задание #2 по теме "Stream API"

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -2,23 +2,25 @@ package ru.courses.main;
 
 import ru.courses.product.*;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics();
 
-        stats.addEntry("https://testpage.ru/num1", 401, "Chrome", "192.168.1.1");
-        stats.addEntry("https://testpage.ru/num2", 200, "Firefox", "192.168.1.2");
-        stats.addEntry("https://testpage.ru/num3", 404, "Chrome", "192.168.1.1");
-        stats.addEntry("https://testpage.ru/num4", 500, "Yandex", "192.168.1.3");
-        stats.addEntry("https://testpage.ru/num5", 404, "Internet Explorer", "192.168.1.1");
+        stats.addEntry("Yandex/5.2", "192.168.1.1", "http://testpage1.ru", 1622547800);
+        stats.addEntry("Mozilla/3.0", "192.168.1.2", null, 1622547803);
+        stats.addEntry("Googlebot/2.1", "192.168.1.5", null, 1622547800); // Это бот, будет проигнорирован
+        stats.addEntry("Yandex/1.0", "192.168.1.1", "http://testpage3.com", 1622547807);
+        stats.addEntry("Mozilla/5.3", "192.168.1.1", "https://testpage2.ru", 1622547801);
 
-        System.out.println("Список страниц с кодом 404: " + stats.getAbsentPages());
-        System.out.println("Статистика браузеров: " + stats.getBrowserStatistics());
+        int peakVisits = stats.getPeakVisits();
+        System.out.println("Максимальное количество посещений за одну секунду: " + peakVisits);
 
-        stats.setTimePeriod(3.87); // Указываем время в часах, за которое есть записи в логах
+        HashSet<String> referrerDomains = stats.getReferrerDomains();
+        System.out.println("Доменные имена рефереров: " + referrerDomains);
 
-        System.out.println("Среднее количество посещений сайта за час: " + stats.averageVisitsPerHour());
-        System.out.println("Среднее количество ошибочных запросов за час: " + stats.averageErrorRequestsPerHour());
-        System.out.println("Средняя посещаемость одним пользователем: " + stats.averageVisitsPerUser());
+        int maxVisitsByUser = stats.getMaxVisitsByUser();
+        System.out.println("Максимальное количество посещений от одного пользователя: " + maxVisitsByUser);
     }
 }
--- src/ru/courses/product/Statistics.java
+++ src/ru/courses/product/Statistics.java
@@ -3,89 +3,61 @@ package ru.courses.product;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Statistics {
-    private Set<String> absentPages;
-    private Map<String, Integer> browserNum;
-    private Set<String> uniqueUserIPs; // уникальные IP не ботов
+    private Map<String, Integer> visitsOfOneUser = new HashMap<>();
+    private HashSet<String> referrerDomains = new HashSet<>();
+    private Map<Integer, Integer> visitsInOneSecond = new HashMap<>();
     private int totalVisits; // кол-во посещений
-    private double hours;   // период времени в часах
-    private int errorRequestsNum; // кол-во ошибочных запросов
+ //   private double hours;   // период времени в часах
+ //   private int errorRequestsNum; // кол-во ошибочных запросов
 
     public Statistics() {
-        absentPages = new HashSet<>();
-        browserNum = new HashMap<>();
-        uniqueUserIPs = new HashSet<>();
         totalVisits = 0;
-        hours = 0;
-        errorRequestsNum = 0;
+   //     hours = 0;
+   //     errorRequestsNum = 0;
     }
 
-    public void addEntry(String url, int responseCode, String browser, String userIP) {
-        if (responseCode == 404) {
-            absentPages.add(url);
+    public void addEntry(String userAgent, String ipAddress, String referer, int timestamp) {
+        if (userAgent != null && userAgent.toLowerCase().contains("bot")) {
+            return;
         }
 
-        if (responseCode >= 400 && responseCode < 600) {
-            errorRequestsNum++;
-        }
+        visitsInOneSecond.put(timestamp, visitsInOneSecond.getOrDefault(timestamp, 0) + 1);
+        visitsOfOneUser.put(ipAddress, visitsOfOneUser.getOrDefault(ipAddress, 0) + 1);
 
-        if (browser != null) {
-            browserNum.put(browser, browserNum.getOrDefault(browser, 0) + 1);
-            if (!browser.toLowerCase().contains("bot")) {
-                totalVisits++;
-                uniqueUserIPs.add(userIP);
+        if (referer != null) {
+            String domain = extractDomain(referer);
+            if (domain != null) {
+                referrerDomains.add(domain);
             }
         }
+
+        totalVisits++;
     }
 
-    public Set<String> getAbsentPages() {
-        return absentPages;
+    public int getPeakVisits() {
+        return visitsInOneSecond.values().stream()
+                .max(Integer::compare)
+                .orElse(0);
     }
 
-    public Map<String, Double> getBrowserStatistics() {
-        Map<String, Double> browserStatistics = new HashMap<>();
-        int totalNum = 0;
+    public HashSet<String> getReferrerDomains() {
+        return referrerDomains;
+    }
 
-        for (int count : browserNum.values()) {
-            totalNum += count;
+    public int getMaxVisitsByUser() {
+        return visitsOfOneUser.values().stream()
+                .max(Integer::compare)
+                .orElse(0);
+    }
+
+    private String extractDomain(String url) {
+        try {
+            java.net.URL aURL = new java.net.URL(url);
+            return aURL.getHost();
+        } catch (Exception e) {
+            return null;
         }
-
-        if (totalNum == 0) {
-            return browserStatistics;
-        }
-
-        for (Map.Entry<String, Integer> entry : browserNum.entrySet()) {
-            String browserName = entry.getKey();
-            int count = entry.getValue();
-            double proportion = (double) count / totalNum;
-            browserStatistics.put(browserName, proportion);
-        }
-        return browserStatistics;
-    }
-
-    public void setTimePeriod(double hours) { // Изменено на double
-        this.hours = hours;
-    }
-
-    public double averageVisitsPerHour() {
-        if (hours == 0) {
-            return 0;
-        }
-        return totalVisits / hours; // Используем double для деления
-    }
-
-    public double averageErrorRequestsPerHour() {
-        if (hours == 0) {
-            return 0;
-        }
-        return errorRequestsNum / hours; // Используем double для деления
-    }
-
-    public double averageVisitsPerUser() {        if (uniqueUserIPs.isEmpty()) {
-        return 0;
-    }
-        return (double) totalVisits / uniqueUserIPs.size();
     }
 }
